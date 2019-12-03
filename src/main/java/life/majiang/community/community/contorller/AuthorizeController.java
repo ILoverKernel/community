394a09b1b26e2b51b2f8a29b1d3dea1d4097c46a
tong <1780311661@qq.com>
2019-12-03 17:00:17 +0800

    code

--- src/main/java/life/majiang/community/community/contorller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/contorller/AuthorizeController.java
@@ -1,13 +1,34 @@
 package life.majiang.community.community.contorller;
 
+import life.majiang.community.community.dto.AccessTokenDTO;
+import life.majiang.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- *
+ *授权登录
  */
 @Controller
 public class AuthorizeController {
-    public String callback(){
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="status")String status){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("Iv1.e66413412bfa19fc");
+        accessTokenDTO.setClient_secret("d0c008a002c109522c274789f1f8f506bd26d3b5");
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(status);
+        githubProvider.getAccessToken(accessTokenDTO);
+
+
+
         return "index";
     }
 }
